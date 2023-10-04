086c87465a431f38ddbed68aaee64ade73c95424
majo06005 <majo06005@E-PW0234K2>
2023-10-04 10:34:05 +0200

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiumet? Svara j för ja och n gör nej.");
+            string svar = Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du?");
+            string åldertext = Console.ReadLine();
+            int ålder = int.Parse(åldertext);
+
+            if (ålder < 22 && svar == "j")
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Tyvärr vi söker annan personal.");
+            }
+             Console.ReadKey();
+        }
+    }
+}
